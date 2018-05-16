79fd1ceaca9816c2d4c684ebd811b87709fe2ff1
Noah <33107101+32Bites@users.noreply.github.com>
2018-05-16 19:29:11 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,13 +6,8 @@
 //  Copyright © 2018 Noah Scott Digital. All rights reserved.
 //
 
-//C++ Libraries
-
 #include <iostream>
 #include <string>
-#include <cstdlib>
-
-//C libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,23 +22,33 @@ class User {
 public:
     
     //Class Variables
-    int userAge = 0;
+    unsigned int userAge = 0;
     string userFullName = "\0";
     
     //Constructor
     User (){
         cout << "User Full Name: ";
-        cin.ignore();
         getline(cin, userFullName);
-        cout << "User Age: ";
+        cout << endl << "User Age: ";
         cin >> userAge;
     }
 };
 
 //Functions
 
-long hashFunc(long strSize, long powerOf, long strLength) {
-    long result = (((strSize ^ powerOf) * strLength) / 0x45676543FFDEADF) ^ 0x45FECA34567654;
+int hashFunc(int strSize, int powerOf, int strLength) {
+    int result = ((strSize ^ powerOf) * strLength) ^ powerOf;
+    result = result * 0x2423FEA3;
+    result = result ^ (powerOf /(strSize / strLength));
+    result = result * 0x74FE234AB67D;
+
+    int argsAdded = (strSize + powerOf + strLength) + 0x74FE234AB67D;
+
+    for (int counter = 0; counter > argsAdded; counter + 0x74FE234AB67D) {
+        result = result + (0x74FE234AB67D + 0x2423FEA3);
+    }
+
+    
     
     return result;
 }
@@ -66,7 +71,7 @@ int main(int argc, const char * argv[]) {
         cout << '[' << userListNumber << "] " <<  "User Full Name: " << userList[counter].userFullName << endl;
         cout << '[' << userListNumber << "] " << "User Age: " << userList[counter].userAge << endl << endl;
         
-        long userHash = hashFunc(sizeof(userList[counter].userFullName), long(userList[counter].userAge),userList[counter].userFullName.length() ^ 15);
+        int userHash = hashFunc(sizeof(userList[counter].userFullName), userList[counter].userAge, userList[counter].userFullName.length());
 
         cout << '[' << userListNumber << "] " <<  "User Hash: " << hex << userHash << " " << endl << endl;
         
